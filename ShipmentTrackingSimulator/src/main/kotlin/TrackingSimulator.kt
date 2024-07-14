0f256278dbba98badbf01f8e8698862370169397
tannerwheeler <tannerleonwheeler@gmail.com>
2024-07-13 19:26:40 -0600

    Added TrackingSimulator.kt

---
+++ ShipmentTrackingSimulator/src/main/kotlin/TrackingSimulator.kt
@@ -0,0 +1,15 @@
+object TrackingSimulator {
+    var shipments = mutableListOf<Shipment>()
+
+    fun findShipment(id: String) {
+        TODO()
+    }
+
+    fun addShipment(shipment: Shipment) {
+        TODO()
+    }
+
+    fun runSimulation() {
+        
+    }
+}
